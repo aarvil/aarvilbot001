4560a2204e90be7ebc86cff714aede47f64c0a58
aarvil <aarvil_git@outlook.com>
2017-01-23 13:40:25 -0600

    Intial commit

---
+++ simplebot.js
@@ -0,0 +1,66 @@
+var builder = require('botbuilder');
+var restify = require('restify');
+
+//=========================================================
+// Bot Setup
+//=========================================================
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+
+// Create chat bot
+// Microsoft App Name: aarvil
+// Microsoft ID: 3510ec80-b995-4d00-8641-c517adeb2851
+// Microsoft App Password: bMimW4uX3YTg5KnSUn385Xe
+  
+// Create chat bot
+var connector = new builder.ChatConnector({
+    //appId: process.env.MICROSOFT_APP_ID,
+    //appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: "3510ec80-b995-4d00-8641-c517adeb2851",
+    appPassword: "bMimW4uX3YTg5KnSUn385Xe"
+});
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+var intents = new builder.IntentDialog();
+bot.dialog('/', intents);
+
+intents.matches(/^change name/i, [
+    function (session) {
+        session.beginDialog('/profile');
+    },
+    function (session, results) {
+        session.send('Ok... Changed your name to %s', session.userData.name);
+    }
+]);
+
+intents.onDefault([
+    function (session, args, next) {
+        if (!session.userData.name) {
+            session.beginDialog('/profile');
+        } else {
+            next();
+        }
+    },
+    function (session, results) {
+        session.send('Hello %s!', session.userData.name);
+    }
+]);
+
+bot.dialog('/profile', [
+    function (session) {
+        builder.Prompts.text(session, 'Hi! What is your name?');
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        session.endDialog();
+    }
+]);
